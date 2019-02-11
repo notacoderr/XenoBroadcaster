11a7a4232ca4b4fd5a076ff01fdc16b7272d22e7
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-02-11 11:58:46 +0800

    Update BroadcastTask.php

--- src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
+++ src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
@@ -2,30 +2,15 @@
 
 namespace Xenophilicy\XenoBroadcaster;
 
+use pocketmine\Server;
 use pocketmine\scheduler\Task;
 
 class BroadcastTask extends Task{
 
-	private $plugInstance;
 
 	public function onRun(int $currentTick)
 	{
-		$this->plugInstance = XenoBroadcaster::getInstance();
-		$exp = $this->plugInstance->exp;
-		if($this->plugInstance->random)
-		{
-			foreach($this->plugInstance->getOnlinePlayers() as $hooman)
-			{
-				$exp = explode(":", $exp);
-				$exp = mt_rand($exp[0], $exp[1]);
-				if($hooman instanceof Player) $this->plugInstance->core->data->addVal($hooman, "exp", $exp);
-			}
-		} else {
-			foreach($this->plugInstance->getOnlinePlayers() as $hooman)
-			{
-				if($hooman instanceof Player) $this->plugInstance->core->data->addVal($hooman, "exp", $exp);
-			}
-		}
+		Server::getInstance()->getPluginManager()->getPlugin("AutoExperience")->timeCheck();
 		$this->plugInstance->broadcastMessage("§l§cS A K U R A ❯ §7All online players received {$exp} ! Stay online!");
 	}
 }
