19ca6c89f23482a934a59a764219a5e11fe41f5d
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-02-11 11:13:38 +0800

    Update XenoBroadcaster.php

--- src/Xenophilicy/XenoBroadcaster/XenoBroadcaster.php
+++ src/Xenophilicy/XenoBroadcaster/XenoBroadcaster.php
@@ -6,30 +6,31 @@ use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\utils\config;
-
 
 class XenoBroadcaster extends PluginBase implements Listener{
 
-	private $config;
 	public $random = false;
 	public $exp;
 	public $core;
-	public static $serverInstance;
+	public $config;
+	private $players = [];
 
     	public function onLoad()
     	{
-		$this->saveDefaultConfig();
-		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
-		$this->config->getAll();
 		$this->getLogger()->info("§eLoading......");
     	}
 	
 	public function onEnable()
 	{
+		$this->saveDefaultConfig();
+		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+		$this->config->getAll();
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		self::$serverInstance = $this;
 		$this->hasValidInterval();
+		$this->core = Server::getInstance()->getPluginManager()->getPlugin("CoreX2");
 	}
 	
 	public function onDisable()
@@ -39,29 +40,62 @@ class XenoBroadcaster extends PluginBase implements Listener{
 
 	private function hasValidInterval() : bool
 	{
-		if(!is_integer($this->config->get("Interval"))){
+		if(!is_integer($this->config->get("interval"))){
 			$this->getLogger()->critical("Invalid interval in the config! Plugin Disabling...");
 			$this->getServer()->getPluginManager()->disablePlugin($this);
 			return false;
 		}
-		elseif(is_integer($this->config->get("Interval"))){
+		elseif(is_integer($this->config->get("interval"))){
 			$this->getLogger()->Info("§6AutoEXP§a has been enabled!");
-			$this->core = Server::getInstance()->getPluginManager()->getPlugin("CoreX2");
-			$this->getScheduler()->scheduleRepeatingTask(new BroadcastTask(), $this->config->get("Interval") * 1200);
-			if($this->config->getNested("Experience.type") == "random")
+			
+			$this->getScheduler()->scheduleRepeatingTask(new BroadcastTask(), $this->config->get("interval") * 1200);
+			if($this->config->getNested("experience.type") == "random")
 			{
 				$this->random = true;
-				$this->exp = $this->config->getNested("Experience.amount_random");
+				$this->exp = $this->config->getNested("experience.amount_random");
 			} else {
-				$this->exp = $this->config->getNested("Experience.amount_fixed");
+				$this->exp = $this->config->getNested("experience.amount_fixed");
 			}
 			return true;
 		}
-		return true;
+		return false;
 	}
-
-	public static function getInstance(){
-		return self::$serverInstance;
+	
+	public function onLine(PlayerJoinEvent $event) : void
+	{
+		$player = $event->getPlayer()->getName();
+		if(!in_array($player, $this->players))
+		{
+			$this->players[] =  $player ;
+			var_dump($this->players[$name]);
+		}
+	}
+	
+	public function offLine(PlayerQuitEvent $event) : void
+	{
+		$player = $event->getPlayer()->getName();
+		if(in_array($player, $this->players))
+		{
+			unset($this->players[$name]);
+		}
+	}
+	
+	public function timeCheck() : void
+	{
+		$time = $this->config->get("interval");
+		foreach($this->players as $name)
+		{
+			$this->players[$name] += 1;
+			var_dump($this->players[$name]);
+			if($this->players[$name] >= $time)
+			{
+				$hooman = $this->getServer()->getPlayerExact($name);
+				if($hooman instanceof Player) $this->core->data->addVal($hooman, "exp", $this->exp);
+				//unset( $this->players[$name] );
+				$this->players[] =  $name;
+				var_dump($this->players[$name]);
+			}
+		}
 	}
 }
 ?>
