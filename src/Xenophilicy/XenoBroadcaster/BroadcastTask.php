2470e6d2851db5f5d2cc61221b92b9c641a37116
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-02-11 08:55:59 +0800

    Update BroadcastTask.php

--- src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
+++ src/Xenophilicy/XenoBroadcaster/BroadcastTask.php
@@ -1,17 +1,4 @@
 <?php
-# MADE BY:
-#  __    __                                          __        __  __  __                     
-# /  |  /  |                                        /  |      /  |/  |/  |                    
-# $$ |  $$ |  ______   _______    ______    ______  $$ |____  $$/ $$ |$$/   _______  __    __ 
-# $$  \/$$/  /      \ /       \  /      \  /      \ $$      \ /  |$$ |/  | /       |/  |  /  |
-#  $$  $$<  /$$$$$$  |$$$$$$$  |/$$$$$$  |/$$$$$$  |$$$$$$$  |$$ |$$ |$$ |/$$$$$$$/ $$ |  $$ |
-#   $$$$  \ $$    $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |$$ |$$ |$$ |      $$ |  $$ |
-#  $$ /$$  |$$$$$$$$/ $$ |  $$ |$$ \__$$ |$$ |__$$ |$$ |  $$ |$$ |$$ |$$ |$$ \_____ $$ \__$$ |
-# $$ |  $$ |$$       |$$ |  $$ |$$    $$/ $$    $$/ $$ |  $$ |$$ |$$ |$$ |$$       |$$    $$ |
-# $$/   $$/  $$$$$$$/ $$/   $$/  $$$$$$/  $$$$$$$/  $$/   $$/ $$/ $$/ $$/  $$$$$$$/  $$$$$$$ |
-#                                         $$ |                                      /  \__$$ |
-#                                         $$ |                                      $$    $$/ 
-#                                         $$/                                        $$$$$$/
 
 namespace Xenophilicy\XenoBroadcaster;
 
@@ -21,20 +8,26 @@ class BroadcastTask extends Task{
 
 	private $plugInstance;
 
-	public function onRun(int $currentTick){
+	public function onRun(int $currentTick)
+	{
 		$this->plugInstance = XenoBroadcaster::getInstance();
-		$prefix = str_replace("&", "§", $this->plugInstance->getConfig()->get("Message-Prefix"));
-		$messages = $this->plugInstance->getConfig()->get("Messages");
-		$message = $messages[array_rand($messages)];
-		$this->plugInstance = XenoBroadcaster::getInstance()->getServer();
-		$message = str_replace("&", "§", $message);
-		$message = str_replace("{break}", "\n", $message);
-		$message = str_replace("{online}", count($this->plugInstance->getOnlinePlayers()), $message);
-		$message = str_replace("{max}", $this->plugInstance->getMaxPlayers(), $message);
-		$message = str_replace("{motd}", $this->plugInstance->getMotd(), $message);
-		$message = str_replace("{tps}", $this->plugInstance->getTicksPerSecond(), $message);
-		$message = str_replace("{api}", $this->plugInstance->getApiVersion(), $message);
-		$this->plugInstance->broadcastMessage($prefix.$message);
+		$exp = $this->plugInstance->exp;
+		if($this->plugInstance->random)
+		{
+			foreach($this->plugInstance->getOnlinePlayers() as $hooman)
+			{
+				$exp = explode(":", $exp);
+				$exp = mt_rand($exp[0], $exp[1]);
+				if($hooman instanceof Player) $this->plugInstance->core->data->addVal($hooman, "exp", $exp);
+			}
+		} else {
+			foreach($this->plugInstance->getOnlinePlayers() as $hooman)
+			{
+				if($hooman instanceof Player) $this->plugInstance->core->data->addVal($hooman, "exp", $exp);
+			}
+		}
+		$this->plugInstance->broadcastMessage("§l§cS A K U R A ❯ §7All online players received {$exp} ! Stay online!");
 	}
 }
+
 ?>
